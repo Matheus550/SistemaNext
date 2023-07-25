645f10f22380f9ec1f5e9ca671cc62f5ba3a6a7e
Matheus Teixeira <matheusteixeira191@gmail.com>
2023-07-25 07:24:35 -0300

    0321

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,43 @@ import Link from 'next/link';
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <h1>Bem-vindo à Página Inicial</h1>
-      <p>Aqui você encontrará algumas informações sobre o projeto.</p>
+    <>
+      <div>
+        <h1>Bem-vindo à Página Inicial</h1>
+        <p>Aqui você encontrará algumas informações sobre o projeto.</p>
 
-      {/* Link para a página de cadastro */}
-      <Link href="/">
-        <a>Acessar Cadastro de Pessoa</a>
-      </Link>
-    </div>
+        {/* Link para a página de cadastro */}
+        <Link href="/cadastro">Acessar Cadastro de Pessoa</Link>
+      </div>
+      <div className="menu">
+        <div className="menu-item">
+          Item 1
+          <div className="submenu">
+            Opção 1
+            Opção 2
+            Opção 3
+          </div>
+        </div>
+        <div className="menu-item">
+          Item 2
+          <div className="submenu">
+            Opção 1
+            Opção 2
+            Opção 3
+          </div>
+        </div>
+        <div className="menu-item">
+          Item 3
+          <div className="submenu">
+            Opção 1
+            Opção 2
+            Opção 3
+          </div>
+        </div>
+      </div>
+    </>
+
+
   );
 };
 
